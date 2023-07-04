4e48a52077c347a9786f4a6a2f9104f75e4d27cb
Andrey <andrey@itsua.org>
2023-07-04 19:22:26 +0300

    mysql session store added

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,35 @@ import { Logger, LoggerErrorInterceptor, PinoLogger } from 'nestjs-pino';
 import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import * as MySQLStore from 'express-mysql-session';
+const mySQLStore = MySQLStore(session);
+
 import { SteamAuthGuard } from './auth/steam.guard';
 
 async function bootstrap() {
-  console.log(
-    'Starting application...',
-    process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  );
   const app = await NestFactory.create(AppModule);
-  app.useLogger(app.get(Logger));
+
+  const logger = app.get(Logger);
+  app.useLogger(logger);
+  logger.log(
+    `STARTING APP... MODE: ${
+      process.env.NODE_ENV === 'production' ? 'production' : 'development'
+    }`,
+  );
+
+  // init mysql table sessions as session store
+  const mysqlSessionStoreOptions = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    clearExpired: true,
+    expiration: 1000 * 60 * 60 * 24 * 2,
+    createDatabaseTable: true,
+  };
+  const sessionStore = new mySQLStore(mysqlSessionStoreOptions);
+
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
   app.use(cookieParser());
   app.use(
@@ -21,6 +41,7 @@ async function bootstrap() {
       name: process.env.SESSION_NAME,
       resave: true,
       saveUninitialized: false,
+      store: sessionStore,
       cookie: {
         secure: process.env.NODE_ENV === 'production',
         maxAge: 1000 * 60 * 60 * 24 * 2, // 2 days cookies
